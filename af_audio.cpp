9b22db3d1f5c5fcb1ee9ad46e51699ab45a4884f
Austin Fox <fostinaux@gmail.com>
2015-04-13 19:23:30 -0700

    audio improvements

--- af_audio.cpp
+++ af_audio.cpp
@@ -2,10 +2,34 @@
 #include "af_audio.hpp"
 
 SDL_AudioDeviceID afDevice = 0;
+afAudioRing afAudioBuffer = {};
+
+float afAudioRingRead(void)
+{
+	afAudioBuffer.laps |= !(afAudioBuffer.readpt & afAudioBuffer.writept);
+	float rv = afAudioBuffer.buffer[afAudioBuffer.readpt++];
+	afAudioBuffer.readpt %= AF_AUDIO_BUFFER_SIZE;
+	return rv;
+}
+
+float audioscale = 1;
+const float audioceiling = 0.4;
 
 void afAudioCallback(void *udata, Uint8 *stream, int len)
 {
-	SDL_memset(stream, 0, len);
+	float raw;
+	float rawsign;
+	for (int i = 0; i < len; i++)
+	{
+		raw = afAudioRingRead();
+		rawsign = (raw < 0)?-1.f:1.f;
+		raw *= rawsign;
+		audioscale = (raw > audioceiling)?
+			audioceiling/(raw * (1/audioceiling)):
+			audioscale + (1-audioscale)/10;
+		stream[i] = raw*rawsign*audioscale;
+		afAudioBuffer.audiotime++;
+	}
 }
 
 bool afAudioInit(const SDL_AudioSpec *want, SDL_AudioSpec *have)
@@ -16,6 +40,9 @@ bool afAudioInit(const SDL_AudioSpec *want, SDL_AudioSpec *have)
 			0,0,
 			&wantd, have,
 			SDL_AUDIO_ALLOW_ANY_CHANGE);
+	afAudioBuffer.samples = have->samples;
+	afAudioBuffer.freq = have->freq;
+	afAudioBuffer.channels = have->channels;
 	SDL_PauseAudioDevice(afDevice,0);
 	return (want->format == have->format);
 }
--- af_audio.hpp
+++ af_audio.hpp
@@ -1,7 +1,26 @@
 #pragma once
 #include "include/SDL_audio.h"
 
-void afAudioCallback(void *udata, Uint8 *stream, int len);
+#define AF_AUDIO_BUFFER_SIZE 8192
+typedef struct {
+	float buffer[AF_AUDIO_BUFFER_SIZE];
+	unsigned int
+		readpt,
+		writept,
+		laps,
+		samples,
+		freq,
+		channels;
+	unsigned long int 
+		audiotime;
+} afAudioRing;
+
+typedef struct {
+} afSquareSynth;
+
+extern afAudioRing afAudioBuffer;
+
+
 
 bool afAudioInit(const SDL_AudioSpec *want, SDL_AudioSpec *have);
 
